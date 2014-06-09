87f1b18c94865e3f2c1e1b82c361b8d873ceeec5
kumarsarak <saravana.k@live.com>
2014-06-09 15:14:58 -0700

    EDM REST API without testing for create and update

---
+++ EDMApiWebRole/Controllers/APInvoiceController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using EDMApiWebRole.Models;
+using System.Diagnostics;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace EDMApiWebRole.Controllers
+{
+    public class APInvoiceController : ApiController
+    {
+        private APAppDBContext db = new APAppDBContext();
+
+        [System.Web.Http.HttpPost]
+        public String Create(string recordnumber, string invoicedate, string invoicenumber, string vendornumber, string vendorname, string ponumber, string invoicetypecd, string filelocation, string invoiceimagename)
+        {
+            String returnvalue;
+            bool insertsuccess = false;
+            try
+            {
+                if (!String.IsNullOrEmpty(recordnumber) && !String.IsNullOrEmpty(invoicenumber))
+                {
+                    var apinvoicerecord = new APInvoice { Record_Number = recordnumber, Invoice_Date = Convert.ToDateTime(invoicedate), Invoice_Number = invoicenumber, Vendor_Number = vendornumber, Vendor_Name = vendorname, PO_Number = ponumber, Invoice_Type_cd = invoicetypecd };
+                    db.APInvoices.Add(apinvoicerecord);
+                    db.SaveChanges();
+                    insertsuccess = true;
+                }
+                returnvalue = "AP MetaData succesfully Added;";
+
+            }
+            catch (Exception ex)
+            {
+                DeleteMetadata(recordnumber);
+                returnvalue = "AP Metadata not saved due to exceptions";
+                return returnvalue;
+            }
+
+            returnvalue = returnvalue + InsertInvoiceIntoBlob(recordnumber, filelocation, invoiceimagename, insertsuccess);
+            return returnvalue;
+        }
+
+        [System.Web.Http.HttpPost]
+        public String Update(string recordnumber, string invoicedate, string invoicenumber, string vendornumber, string vendorname, string ponumber, string invoicetypecd, string filelocation, string invoiceimagename)
+        {
+            String returnvalue;
+            try
+            {
+                if (!String.IsNullOrEmpty(recordnumber))
+                {
+                    var apinvoicerecord = db.APInvoices.Single(x => x.Record_Number == recordnumber);
+                    if (apinvoicerecord != null)
+                    {
+
+                        apinvoicerecord.Invoice_Date = (!String.IsNullOrEmpty(invoicedate)) ? Convert.ToDateTime(invoicedate) : apinvoicerecord.Invoice_Date;
+                        apinvoicerecord.Invoice_Number = (!String.IsNullOrEmpty(invoicenumber)) ? invoicenumber : apinvoicerecord.Invoice_Number;
+                        apinvoicerecord.Vendor_Number = (!String.IsNullOrEmpty(vendornumber)) ? vendornumber : apinvoicerecord.Vendor_Number;
+                        apinvoicerecord.Vendor_Name = (!String.IsNullOrEmpty(vendorname)) ? vendorname : apinvoicerecord.Vendor_Name;
+                        apinvoicerecord.PO_Number = (!String.IsNullOrEmpty(ponumber)) ? ponumber : apinvoicerecord.PO_Number;
+                        apinvoicerecord.Invoice_Type_cd = (!String.IsNullOrEmpty(invoicetypecd)) ? invoicetypecd : apinvoicerecord.Invoice_Type_cd;
+                    }
+                    DeleteMetadata(recordnumber);
+                    db.APInvoices.Add(apinvoicerecord);
+                    db.SaveChanges();
+                    returnvalue = "AP MetaData succesfully Updated";
+                }
+                else
+                {
+                    returnvalue = "AP MetaData not Updated as the recordnumber is empty";
+
+                }
+                 
+            }
+            catch (Exception ex)
+            {
+                DeleteMetadata(recordnumber);
+                returnvalue = "AP Metadata not updated due to exceptions";
+                return returnvalue;
+            }
+
+            return returnvalue;
+
+        }
+
+        private String InsertInvoiceIntoBlob(string recordnumber, string filelocation, string invoiceimagename, bool insertsuccess)
+        {
+            try
+            {
+                
+                if (insertsuccess)
+                {
+                    // Retrieve reference to a blob named "blobName".
+                    CloudBlockBlob blockBlob = GetBlockBlob(recordnumber);
+                    // Create or overwrite the "blobName" blob with contents from a local file.
+                    using (var fileStream = System.IO.File.OpenRead(filelocation))
+                    {
+                        // Set content type accordingly
+                        if (invoiceimagename.ToLower().EndsWith(".pdf"))
+                        {
+                            blockBlob.Properties.ContentType = "application/pdf";
+                        }
+                        else if (invoiceimagename.ToLower().EndsWith(".tif"))
+                        {
+                            blockBlob.Properties.ContentType = "image/tiff";
+                        }
+                        //Upload the image
+                        blockBlob.UploadFromStream(fileStream);
+                    }
+
+                    return "AP Invoice Image successfully saved";
+                }
+                else
+                {
+                    return "AP Invoice Image not Saved due to errors in Metadata";
+                }
+            }
+            catch (Exception ex)
+            {
+                DeleteMetadata(recordnumber);
+                DeleteImage(recordnumber);
+                return "AP Invoice Image and Metadata not saved due to exceptions";
+            }
+            
+        }
+
+        private void DeleteMetadata(string recordnumber)
+        {
+            if (!String.IsNullOrEmpty(db.APInvoices.Single(x => x.Record_Number == recordnumber).Record_Number))
+            {
+                db.APInvoices.Remove(db.APInvoices.Single(x => x.Record_Number == recordnumber));
+                db.SaveChanges();
+            }
+
+        }
+
+        private void DeleteImage(string recordnumber)
+        {
+            CloudBlockBlob blockBlob = GetBlockBlob(recordnumber);
+            blockBlob.Delete();
+
+        }
+
+        private CloudBlockBlob GetBlockBlob(string recordnumber)
+        {
+            // Retrieve AP storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["APStorageConnectionString"].ConnectionString);
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            String containerName = ConfigurationManager.ConnectionStrings["APContainerConnectionString"].ConnectionString;
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            return container.GetBlockBlobReference(recordnumber);
+        }
+    }
+}
---
+++ EDMApiWebRole/Controllers/ARCustRemitController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using EDMApiWebRole.Models;
+using System.Diagnostics;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace EDMApiWebRole.Controllers
+{
+    public class ARCustRemitController : ApiController
+    {
+        private ARAppDBContext db = new ARAppDBContext();
+
+        [System.Web.Http.HttpPost]
+        public String Create(string recordnumber, string checkdepositdate, string checkserialnum, string checktransitnum, string checkaccountnum, string lockbox, string hasactionitem, string lawsoncustomer, string filelocation, string checkimagename)
+        {
+            String returnvalue;
+            bool insertsuccess = false;
+            try
+            {
+                if (!String.IsNullOrEmpty(recordnumber) && !String.IsNullOrEmpty(checkaccountnum))
+                {
+                    var arcustomerremitrecord = new ARCustRemit { Record_Number = recordnumber, Chk_Deposit_Dt = Convert.ToDateTime(checkdepositdate), Chk_Serial_Num = checkserialnum, Chk_Transit_Num = checktransitnum, Chk_Account_Num = checkaccountnum, Lockbox = lockbox, Has_Action_Item = hasactionitem, Lawson_Customer = lawsoncustomer };
+                    db.ARCustRemits.Add(arcustomerremitrecord);
+                    db.SaveChanges();
+                    insertsuccess = true;
+                }
+                returnvalue = "AR MetaData succesfully Added;";
+
+            }
+            catch (Exception ex)
+            {
+                DeleteMetadata(recordnumber);
+                returnvalue = "AR Metadata not saved due to exceptions";
+                return returnvalue;
+            }
+
+            returnvalue = returnvalue + InsertCheckIntoBlob(recordnumber, filelocation, checkimagename, insertsuccess);
+            return returnvalue;
+        }
+
+        [System.Web.Http.HttpPost]
+        public String Update(string recordnumber, string checkdepositdate, string checkserialnum, string checktransitnum, string checkaccountnum, string lockbox, string hasactionitem, string lawsoncustomer)
+        {
+            String returnvalue;
+            try
+            {
+                if (!String.IsNullOrEmpty(recordnumber))
+                {
+                    var arcustomerremitrecord = db.ARCustRemits.Single(x => x.Record_Number == recordnumber);
+                    if (arcustomerremitrecord != null)
+                    {
+
+                        arcustomerremitrecord.Chk_Deposit_Dt = (!String.IsNullOrEmpty(checkdepositdate)) ? Convert.ToDateTime(checkdepositdate) : arcustomerremitrecord.Chk_Deposit_Dt;
+                        arcustomerremitrecord.Chk_Serial_Num = (!String.IsNullOrEmpty(checkserialnum)) ? checkserialnum : arcustomerremitrecord.Chk_Serial_Num;
+                        arcustomerremitrecord.Chk_Transit_Num = (!String.IsNullOrEmpty(checktransitnum)) ? checktransitnum : arcustomerremitrecord.Chk_Transit_Num;
+                        arcustomerremitrecord.Chk_Account_Num = (!String.IsNullOrEmpty(checkaccountnum)) ? checkaccountnum : arcustomerremitrecord.Chk_Account_Num;
+                        arcustomerremitrecord.Lockbox = (!String.IsNullOrEmpty(lockbox)) ? lockbox : arcustomerremitrecord.Lockbox;
+                        arcustomerremitrecord.Has_Action_Item = (!String.IsNullOrEmpty(hasactionitem)) ? hasactionitem : arcustomerremitrecord.Has_Action_Item;
+                        arcustomerremitrecord.Lawson_Customer = (!String.IsNullOrEmpty(lawsoncustomer)) ? lawsoncustomer : arcustomerremitrecord.Lawson_Customer;
+                    }
+                    DeleteMetadata(recordnumber);
+                    db.ARCustRemits.Add(arcustomerremitrecord);
+                    db.SaveChanges();
+                    returnvalue = "AR MetaData succesfully Updated";
+                }
+                else
+                {
+                    returnvalue = "AR MetaData not Updated as the recordnumber is empty";
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DeleteMetadata(recordnumber);
+                returnvalue = "AR Metadata not updated due to exceptions";
+                return returnvalue;
+            }
+
+            return returnvalue;
+
+        }
+
+        private String InsertCheckIntoBlob(string recordnumber, string filelocation, string checkimagename, bool insertsuccess)
+        {
+            try
+            {
+
+                if (insertsuccess)
+                {
+                    // Retrieve reference to a blob named "blobName".
+                    CloudBlockBlob blockBlob = GetBlockBlob(recordnumber);
+                    // Create or overwrite the "blobName" blob with contents from a local file.
+                    using (var fileStream = System.IO.File.OpenRead(filelocation))
+                    {
+                        // Set content type accordingly
+                        if (checkimagename.ToLower().EndsWith(".pdf"))
+                        {
+                            blockBlob.Properties.ContentType = "application/pdf";
+                        }
+                        else if (checkimagename.ToLower().EndsWith(".tif"))
+                        {
+                            blockBlob.Properties.ContentType = "image/tiff";
+                        }
+                        //Upload the image
+                        blockBlob.UploadFromStream(fileStream);
+                    }
+
+                    return "AR Customer Remit Image successfully saved";
+                }
+                else
+                {
+                    return "AR Customer Remit Image not Saved due to errors in Metadata";
+                }
+            }
+            catch (Exception ex)
+            {
+                DeleteMetadata(recordnumber);
+                DeleteImage(recordnumber);
+                return "AR Customer Remit Image and Metadata not saved due to exceptions";
+            }
+
+        }
+
+        private void DeleteMetadata(string recordnumber)
+        {
+            if (!String.IsNullOrEmpty(db.ARCustRemits.Single(x => x.Record_Number == recordnumber).Record_Number))
+            {
+                db.ARCustRemits.Remove(db.ARCustRemits.Single(x => x.Record_Number == recordnumber));
+                db.SaveChanges();
+            }
+
+        }
+
+        private void DeleteImage(string recordnumber)
+        {
+            CloudBlockBlob blockBlob = GetBlockBlob(recordnumber);
+            blockBlob.Delete();
+
+        }
+
+        private CloudBlockBlob GetBlockBlob(string recordnumber)
+        {
+            // Retrieve AP storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["ARStorageConnectionString"].ConnectionString);
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            String containerName = ConfigurationManager.ConnectionStrings["ARContainerConnectionString"].ConnectionString;
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            return container.GetBlockBlobReference(recordnumber);
+        }
+    }
+}
